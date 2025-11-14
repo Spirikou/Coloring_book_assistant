79d69bd47fe27ab14d305310a4d5733598835905
Spirikou <alexandre.spiri@hotmail.com>
2025-11-14 15:01:03 +0000

    small updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import os
 import json
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
-from langchain.schema import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 # Load environment variables
 load_dotenv()
